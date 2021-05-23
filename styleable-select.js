469e569201ce9fd925122aeabc239fe130ee7076
César Salgueiro <csalgueiro.dev@gmail.com>
2021-05-23 22:16:03 +0200

    initElement function with click listener to show list

--- styleable-select.js
+++ styleable-select.js
@@ -4,8 +4,8 @@ export default class StyleableSelect {
     this.optionsList = getOptionsList(element.querySelectorAll("option"))
     this.element = document.createElement("div")
     this.label = document.createElement("span")
-    this.optionsCustomElement = document.createElement("ul")
-    //initElement(this)
+    this.optionsElement = document.createElement("ul")
+    initElement(this)
     
     //Hide select and add custom styleable element to the DOM
     element.style.display = "none"
@@ -22,6 +22,35 @@ export default class StyleableSelect {
 
 }
 
+function initElement(select) {
+  //Adds classes to new elements
+  select.element.classList.add("styleable-select-container")
+  select.element.tabIndex = 0
+
+  select.label.classList.add("styleable-select-value")
+  select.label.innerText = select.selectedOption.label
+  select.element.append(select.label)
+
+  select.optionsElement.classList.add("styleable-select-options")
+
+  //Adds optionsList to optionsElement
+  select.optionsList.forEach(option => {
+    const optionElement = document.createElement("li")
+    optionElement.classList.add("styleable-select-option")
+    optionElement.classList.toggle("selected", option.selected)
+
+    optionElement.innerText = option.label
+    optionElement.dataset.value = option.value
+    optionElement.addEventListener("click", () => {
+      select.selectValue(option.value)
+      select.optionsElement.classList.remove("show")
+    })
+    select.optionsElement.append(optionElement)
+  })
+  select.element.append(select.optionsElement)
+
+  
+}
 
 function getOptionsList(optionElements) {
   //Convert to array and map into array object
